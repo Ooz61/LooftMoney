8ecb08d8eaa2f1284bdc1aa93281e01b6ad956c5
Stanislav Kushnarev <zoreks6195@gmail.com>
2020-10-23 23:04:52 +0300

    Added Hello

---
+++ app/src/main/java/com/example/looftmoney/AddItemActivity.java
@@ -0,0 +1,28 @@
+package com.example.looftmoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    Button btnSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+
+        btnSend = findViewById(R.id.send);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "На меня нажали", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
